4325736f7743e28ff6b8308f282e30677ed7107c
Unknown <patrick.krauss@outlook.com>
2018-05-02 05:16:21 -0300

    Finished camera swipe movimentation

---
+++ Assets/Game/Scripts/CameraMovimentation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Lean.Touch;
+
+public class CameraMovimentation : MonoBehaviour
+{
+
+    [Tooltip("Pull force of the swipe")]
+    public int pullForce = 20;
+     private Rigidbody rb;
+     private Vector3 direction;
+     void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable(){
+        InputManager.OnSwiped += moveCamera;
+    }
+
+    private void OnDisable(){
+        InputManager.OnSwiped -= moveCamera;
+    }
+    private void moveCamera(LeanFinger finger, Vector2 delta)
+    {
+        direction.Set(delta.x,0,delta.y);
+        rb.AddForce(-direction*pullForce);
+    }
+
+}
