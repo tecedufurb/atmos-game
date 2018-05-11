768bdfcbfbdebec3b3fcb7b200d010d970d69d94
Unknown <patrick.krauss@outlook.com>
2018-05-11 18:30:18 -0300

    Implemented zoom action using events

--- Assets/_Scripts/CameraMovimentation.cs
+++ Assets/_Scripts/CameraMovimentation.cs
@@ -3,14 +3,42 @@ using UnityEngine.Events;
 using Lean.Touch;
 
 public class CameraMovimentation : MonoBehaviour{
-     private Rigidbody rb;
-     private Vector3 direction;
+    private Rigidbody rigidbody;
+    private Camera camera;
+    private Vector3 direction;
+    
     [Tooltip("Strenght of the swipe"),SerializeField]
-     private int pushForce;
+    private int pushForce;
+
+    [Header("Zoom Settings"),Tooltip("Ignore fingers with StartedOverGui?")]
+    public bool IgnoreGuiFingers = true;
+    
+    [Tooltip("The current FOV/Size"),SerializeField]
+    private float CurrentZoom;
+
+    [Tooltip("The minimum FOV we want to zoom to"),SerializeField]
+    private float ZoomMin;
+
+    [Tooltip("The maximum FOV we want to zoom to"),SerializeField]
+    private float ZoomMax;
 
      void Awake(){
-         pushForce = 50;
-        rb = GetComponent<Rigidbody>();
+        rigidbody = GetComponent<Rigidbody>();
+        camera = GetComponent<Camera>();
+        CurrentZoom = (camera.orthographic==true) ? camera.orthographicSize : camera.fieldOfView;
+        ZoomMin = 40;
+        ZoomMax= 100;
+        pushForce = 30;
+    }
+    void Update(){
+        
+        // Modify the zoom value
+        CurrentZoom *= LeanGesture.GetPinchRatio(LeanTouch.GetFingers(IgnoreGuiFingers));
+    
+        CurrentZoom = Mathf.Clamp(CurrentZoom, ZoomMin, ZoomMax);
+
+        // Set the new zoom
+        zoomCamera(CurrentZoom);
     }
 
     private void OnEnable(){
@@ -22,7 +50,17 @@ public class CameraMovimentation : MonoBehaviour{
     }
     private void moveCamera(LeanFinger finger, Vector2 delta){
         direction.Set(-delta.x,0,-delta.y);
-        rb.AddForce(direction*pushForce);
+        rigidbody.AddForce(direction*pushForce);
     }
 
+    private void zoomCamera(float current){
+        if (camera != null){
+            if (camera.orthographic == true){
+                camera.orthographicSize = current;
+            }
+            else{
+                camera.fieldOfView = current;
+            }
+        }
+    }
 }
