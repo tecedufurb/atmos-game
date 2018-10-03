7d5d18bd1532fe1804c46be3fd91f7528aa28f57
Unknown <patrick.krauss@outlook.com>
2018-10-03 16:49:03 -0300

    Adjust to naming of variables in cameraMovimentation

--- Assets/_Game/Scripts/CameraMovimentation.cs
+++ Assets/_Game/Scripts/CameraMovimentation.cs
@@ -1,62 +1,62 @@
 ﻿using Lean.Touch;
 using UnityEngine;
 
-public class CameraMovimentation : MonoBehaviour{
-    private Rigidbody rigidbody;
-    private Camera camera;
+public class CameraMovimentation : MonoBehaviour {
+
+    private Rigidbody cameraRigidbody;
+    private Camera thisCamera;
     private Vector3 direction;
     private float CurrentZoom;
 
-    [Tooltip("Strenght of the swipe"),SerializeField]
+    [Tooltip("Strenght of the swipe"), SerializeField]
     private int pushForce;
 
-    [Header("Zoom Settings"),Tooltip("Ignore fingers with StartedOverGui?")]
+    [Header("Zoom Settings"), Tooltip("Ignore fingers with StartedOverGui?")]
     public bool IgnoreGuiFingers = true;
 
-    [Tooltip("The minimum FOV we want to zoom to"),SerializeField]
+    [Tooltip("The minimum FOV we want to zoom to"), SerializeField]
     private float MinZoom;
 
-    [Tooltip("The maximum FOV we want to zoom to"),SerializeField]
+    [Tooltip("The maximum FOV we want to zoom to"), SerializeField]
     private float MaxZoom;
 
-     void Awake(){
-        rigidbody = GetComponent<Rigidbody>();
-        camera = GetComponent<Camera>();
-        CurrentZoom = (camera.orthographic==true) ? camera.orthographicSize : camera.fieldOfView;
+    void Awake() {
+        cameraRigidbody = GetComponent<Rigidbody>();
+        thisCamera = GetComponent<Camera>();
+        CurrentZoom = (thisCamera.orthographic == true) ? thisCamera.orthographicSize : thisCamera.fieldOfView;
     }
-    
-    void Update(){
-        
+
+    void Update() {
         // Modify the zoom value
         CurrentZoom *= LeanGesture.GetPinchRatio(LeanTouch.GetFingers(IgnoreGuiFingers));
-    
+
         CurrentZoom = Mathf.Clamp(CurrentZoom, MinZoom, MaxZoom);
 
         // Set the new zoom
         zoomCamera(CurrentZoom);
     }
 
-    private void OnEnable(){
+    private void OnEnable() {
         InputManager.OnSwipe += moveCamera;
     }
 
-    private void OnDisable(){
+    private void OnDisable() {
         InputManager.OnSwipe -= moveCamera;
     }
 
-    private void moveCamera(LeanFinger finger, Vector2 delta){
-        direction.Set(-delta.x,0,-delta.y);
-        rigidbody.AddForce(direction*pushForce);
+    private void moveCamera(LeanFinger finger, Vector2 delta) {
+        direction.Set(-delta.x, 0, -delta.y);
+        cameraRigidbody.AddForce(direction * pushForce);
     }
 
-    private void zoomCamera(float current){
-        if (camera != null){
-            if (camera.orthographic == true){
-                camera.orthographicSize = current;
-            }
-            else{
-                camera.fieldOfView = current;
+    private void zoomCamera(float current) {
+        if (thisCamera != null) {
+            if (thisCamera.orthographic == true) {
+                thisCamera.orthographicSize = current;
+            } else {
+                thisCamera.fieldOfView = current;
             }
         }
     }
+
 }
