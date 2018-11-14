bb85aed9ff793339cc346041731bf96d32c81a91
Unknown <patrick.krauss@outlook.com>
2018-11-14 16:41:56 -0200

    Added vanish action to seeds

--- Assets/_Game/Scripts/Plants/SeedController.cs
+++ Assets/_Game/Scripts/Plants/SeedController.cs
@@ -1,10 +1,46 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class SeedController : MonoBehaviour {
 
-	public void collectSeed() {
-		print("seed destroyed");
-		Destroy(gameObject);
-		// play animation, destroy this seed and increase the qtd of seeds of the player
-	}
+    private MeshRenderer meshRenderer;
+    private int ticks = 15;
+    private readonly float blinkDuration = 0.3f;
+
+    private float aux = 0.06f;
+    private float intervalBetweenBlinks = 1f;
+
+    void Start() {
+        meshRenderer = GetComponent<MeshRenderer>();
+        StartCoroutine(updateSeed());
+    }
+
+    public void collectSeed() {
+        destroySeed();
+        // TODO play animation, increase the qtd of seeds of the player
+    }
+
+    private IEnumerator updateSeed() { // make seed blink until is collected or vanish
+
+        while (ticks >= 0) {
+            StartCoroutine(blink(blinkDuration));
+            ticks--;
+            aux = aux * 1.1f;
+            intervalBetweenBlinks = intervalBetweenBlinks - aux;
+            yield return new WaitForSeconds(intervalBetweenBlinks + blinkDuration);
+        }
+
+        destroySeed();
+    }
+
+    private void destroySeed() {
+        Destroy(gameObject);
+    }
+
+    private IEnumerator blink(float duration) {
+        meshRenderer.enabled = false;
+        yield return new WaitForSeconds(duration);
+        meshRenderer.enabled = true;
+    }
+
 }
